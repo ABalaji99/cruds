32d7277251e4d6dc96b15419e5a5ecf8e0ac321b
A Balaji <amulunarayanabalaji@gmail.com>
2022-11-28 12:53:29 +0530

    FInal changes of logics

---
+++ src/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  NavLink } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <nav>
+        <NavLink to='/'>HOME</NavLink>
+    </nav>
+  )
+}
+
+export default Navigation
